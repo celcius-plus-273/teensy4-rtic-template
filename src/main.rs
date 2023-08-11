6bbf8e17f1539aa878902469e1c0ceb6d7060d75
celcius-plus-273 <kelvinandreshau@gmail.com>
2023-08-10 22:35:46 -0400

    full template for a basic rtic 2.0 teensy program

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,124 @@
-// //! The starter code slowly blinks the LED, sets up
-//! USB logging, and creates a UART driver using pins
-//! 14 and 15. The UART baud rate is [`UART_BAUD`].
-//!
-//! Despite targeting the Teensy 4.0, this starter code
-//! also works on the Teensy 4.1.
-
 #![no_std]
 #![no_main] // bsp-rt is used as the entry point of the program instead
+#![feature(type_alias_impl_trait)] // this feature is needed for RTIC v2
 
-/// BASIC BSP PACKAGES ///
+//// BASIC BSP PACKAGES ///
 use bsp::board;
 use teensy4_bsp as bsp;
 use teensy4_panic as _;
 //////////////////////////
 
-// struct that is used to initalize a blocking delay
-use bsp::hal::timer::Blocking;
+//// RTIC PKACAGES ///
+use rtic::app;
+use rtic_monotonics::systick::*;
+////////////////////
 
-// use local example driver package
-use example_driver::increment;
+// local example driver
+use example_driver;
 
-// delay in miliseconds
-const DELAY_MS: u32 = 500;
+//// THE APP MODULE ///
+//// device: board support package
+//// perihperals: ...?
+//// dispatchers: interrupt handlers for software defined tasks
+//////////////////////
+#[app(device = bsp, peripherals = true, dispatchers = [GPT1])]
+mod app {
+    // this allows us to define our packages outside the app module
+    // we're essetially "bringing them all in"
+    use super::*;
 
-#[bsp::rt::entry]
-fn main() -> ! {
-    // These are peripheral instances. Let the board configure these for us.
-    // This function can only be called once!
-    let instances = board::instances();
+    // accounts for our syst_clock to be in 10 kHz (normal is 1 kHz)
+    // this means that the granularity for the delay is 0.1 ms per tick
+    // therefore we multiply our delay time by a factor of 10
+    const SYST_MONO_FACTOR: u32 = 10;
 
-    // Driver resources that are configured by the board. For more information,
-    // see the `board` documentation.
-    let board::Resources {
-        // `pins` has objects that represent the physical pins. The object
-        // for pin 13 is `p13`.
-        pins,
-        // This is a hardware timer. We'll use it for blocking delays.
-        mut gpt1,
-        // These are low-level USB resources. We'll pass these to a function
-        // that sets up USB logging.
-        usb,
-        // This is the GPIO2 port. We need this to configure the LED as a
-        // GPIO output.
-        mut gpio2,
-        ..
-    } = board::t40(instances);
-
-    // When this returns, you can use the `log` crate to write text
-    // over USB. Use either `screen` (macOS, Linux) or PuTTY (Windows)
-    // to visualize the messages from this example.
-    bsp::LoggingFrontend::default_log().register_usb(usb);
-
-    // This configures the LED as a GPIO output.
-    let led = board::led(&mut gpio2, pins.p13);
-
-    // Configures the GPT1 timer to run at GPT1_FREQUENCY. See the
-    // constants below for more information.
-    gpt1.disable();
-    gpt1.set_divider(GPT1_DIVIDER);
-    gpt1.set_clock_source(GPT1_CLOCK_SOURCE);
-
-    // Convenience for blocking delays.
-    let mut delay = Blocking::<_, GPT1_FREQUENCY>::from_gpt(gpt1);
-
-    let mut counter: u32 = 0;
-    loop {
-        led.toggle();
-        log::info!("Hello from the USB logger! The count is {counter}");
-
-        delay.block_ms(DELAY_MS);
-        counter = increment(counter);
+    // delay in miliseconds
+    const DELAY_MS: u32 = SYST_MONO_FACTOR * 1000;
+    
+    // struct that holds local resources which can be accessed via the context
+    #[local]
+    struct Local {
+        led: board::Led,
     }
+
+    // struct that holds shared resources which can be accessed via the context
+    #[shared]
+    struct Shared {
+        counter: u32,
+    }
+
+    // entry point of the "program"
+    #[init]
+    fn init(cx: init::Context) -> (Shared, Local) {
+        // allocate the resources needed
+        let board::Resources {
+            // usedd to acces pin names
+            pins,
+            // used to control any pin from the gpio2 register
+            // (e.g. pin13 for the on board LED)
+            mut gpio2,
+            // for usb logging :)
+            usb,
+            ..
+        } = board::t40(cx.device);
+
+        // usb logging setup
+        bsp::LoggingFrontend::default_log().register_usb(usb);
+
+        // systick monotonic setup
+        let systick_token = rtic_monotonics::create_systick_token!();
+        Systick::start(cx.core.SYST, 36_000_000, systick_token);
+
+        // init led from gpio2
+        let led = board::led(&mut gpio2, pins.p13);
+
+        // init counter shared variable
+        let counter = 0;
+
+        // spawn a toggle call
+        toggle::spawn().unwrap();
+
+        // return the local, and shared resources to be used from the context
+        (
+            Shared {counter},
+            Local {led}
+        )
+    }
+
+    // lowest priority tasks that runs only while no other task is running
+    #[idle]
+    fn idle(_: idle::Context) -> !{
+        loop {
+            // wfi: wait-for-interrupt
+            cortex_m::asm::wfi();
+        }
+    }
+
+    #[task(local = [led], shared = [counter], priority = 1)]
+    async fn toggle(cx : toggle::Context) {
+        // just renaming our shared variable into a local variable so it's easier to read
+        let mut counter = cx.shared.counter;
+
+        // infinite loop which is allowed as it contains a delay followed by a ".await"
+        loop {
+            // example locking the shared counter variable and updating it's value!
+            counter.lock(|counter| {
+                // increment the counter using an external function
+                *counter = example_driver::increment(*counter);
+                
+                // prints "blink!" to the usb serial port
+                log::info!("blink # {}!", *counter);
+            });
+
+            // toggle the led
+            cx.local.led.toggle();
+
+            // generate a delay using the initialized systick monotonic
+            // by calling the Systick::delay() function
+            Systick::delay(DELAY_MS.millis()).await;
+        }
+
+    }
+
 }
 
-// We're responsible for configuring our timers.
-// This example uses PERCLK_CLK as the GPT1 clock source,
-// and it configures a 1 KHz GPT1 frequency by computing a
-// GPT1 divider.
-use bsp::hal::gpt::ClockSource;
-
-/// The intended GPT1 frequency (Hz).
-const GPT1_FREQUENCY: u32 = 1_000;
-/// Given this clock source...
-const GPT1_CLOCK_SOURCE: ClockSource = ClockSource::HighFrequencyReferenceClock;
-/// ... the root clock is PERCLK_CLK. To configure a GPT1 frequency,
-/// we need a divider of...
-const GPT1_DIVIDER: u32 = board::PERCLK_FREQUENCY / GPT1_FREQUENCY;
